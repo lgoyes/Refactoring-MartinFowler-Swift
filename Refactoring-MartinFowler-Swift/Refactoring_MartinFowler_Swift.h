3d326a64d39a67c79aa62f35a5885a07a88e629a
Luis Goyes <lgoyes@veritran.com>
2023-12-12 18:43:25 -0500

    Initial Commit

---
+++ Refactoring-MartinFowler-Swift/Refactoring_MartinFowler_Swift.h
@@ -0,0 +1,18 @@
+//
+//  Refactoring_MartinFowler_Swift.h
+//  Refactoring-MartinFowler-Swift
+//
+//  Created by Luis David Goyes on 12/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Refactoring_MartinFowler_Swift.
+FOUNDATION_EXPORT double Refactoring_MartinFowler_SwiftVersionNumber;
+
+//! Project version string for Refactoring_MartinFowler_Swift.
+FOUNDATION_EXPORT const unsigned char Refactoring_MartinFowler_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Refactoring_MartinFowler_Swift/PublicHeader.h>
+
+
