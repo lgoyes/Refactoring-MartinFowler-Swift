28afe3d21e3aa09c3f4b52fe2d33bf2714b349b4
Luis David Goyes Garces <luis.goyes117@gmail.com>
2024-03-02 10:57:43 -0500

    Splitphase on default bill printer

--- RefactoringMartinFowler/DefaultBillPrinter.swift
+++ RefactoringMartinFowler/DefaultBillPrinter.swift
@@ -26,6 +26,19 @@ enum Error: Swift.Error {
     case unknownType(String)
 }
 
+struct Bill {
+    let customer: String
+    let items: [BillLineItem]
+    let totalAmountInCents: Int
+    let creditsEarned: Int
+}
+
+struct BillLineItem {
+    let playName: String
+    let amountInCents: Int
+    let audience: Int
+}
+
 protocol BillPrinter {
     func statement() throws -> String
 }
@@ -40,21 +53,33 @@ class DefaultBillPrinter: BillPrinter {
     }
     
     func statement() throws -> String {
-        var totalAmount = 0
-        var volumeCredits = 0
-        var result = "Statement for \(invoice.customer)\n"
-        
-        for performance in invoice.performances {
-            volumeCredits += try CreditsCalculator(playResolver: playResolver, performance: performance).calculate()
-            
-            let play = try playResolver.getPlay(for: performance.playId)
-            let thisAmount = try ChargeCalculator(playResolver: playResolver, performance: performance).calculate()
-            result += "   \(play.name): \( try USDFormatter(amountInCents: thisAmount).format() ) (\(performance.audience)) seats\n"
-            totalAmount += thisAmount
+        let bill = try createBill()
+        return try renderPlainText(bill)
+    }
+    
+    func createBill() throws -> Bill {
+        let items = try invoice.performances.map {
+            let play = try playResolver.getPlay(for: $0.playId)
+            let performanceAmount = try ChargeCalculator(playResolver: playResolver, performance: $0).calculate()
+            return BillLineItem(playName: play.name, amountInCents: performanceAmount, audience: $0.audience)
         }
-        
-        result += "Amount owed is \( try USDFormatter(amountInCents: totalAmount).format() )\n"
-        result += "You earned \(volumeCredits) credits"
+        let totalAmountInCents = items.reduce(0) { partialResult, item in
+            return partialResult + item.amountInCents
+        }
+        let creditsEarned = try invoice.performances.reduce(0) { partialResult, performance in
+            let performanceCredits = try CreditsCalculator(playResolver: playResolver, performance: performance).calculate()
+            return partialResult + performanceCredits
+        }
+        return Bill(customer: invoice.customer, items: items, totalAmountInCents: totalAmountInCents, creditsEarned: creditsEarned)
+    }
+    
+    func renderPlainText(_ bill: Bill) throws -> String {
+        var result = "Statement for \(bill.customer)\n"
+        for item in bill.items {
+            result += "   \(item.playName): \( try USDFormatter(amountInCents: item.amountInCents).format() ) (\(item.audience)) seats\n"
+        }
+        result += "Amount owed is \( try USDFormatter(amountInCents: bill.totalAmountInCents).format() )\n"
+        result += "You earned \(bill.creditsEarned) credits"
         
         return result
     }
